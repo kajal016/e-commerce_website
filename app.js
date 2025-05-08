448c2c8432d941f104126c60c8c93c8c636568cb
kajal016 <145201806+kajal016@users.noreply.github.com>
2025-05-08 11:26:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 
+const lineContainer = document.querySelector(".line-container");
+const menu = document.querySelector(".menu");
+
+lineContainer.addEventListener("click", () => {
+  lineContainer.classList.toggle("active");
+  menu.classList.toggle("active");
+});
